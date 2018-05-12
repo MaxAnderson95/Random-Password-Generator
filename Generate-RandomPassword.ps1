c24882ff6a887414b12c5e4498ca784c038a870b
Max Anderson <cctpmax@gmail.com>
2018-05-12 11:09:20 -0400

    Added length vs number of character sets chosen checking. This ensures that durring verification later on that an infinite loop does not occur.

--- Generate-RandomPassword.ps1
+++ Generate-RandomPassword.ps1
@@ -35,28 +35,45 @@ $AmbiguousCharacters = @('(',')','[',']','{','}','/','\','.',',')
 #Construct a character Set. An array list is used for easy removal of items from array
 [System.Collections.ArrayList]$CharacterSet = @()
 
+#Check if each possible character type has been chosen. 
 If ($LowerCase) {
     
+    #If it has, add it to the character set and increment the character set count by 1
     $CharacterSet += $LowerCaseCharacters
+    $CharacterSetCount++
 
 }
 
 If ($UpperCase) {
 
+    #If it has, add it to the character set and increment the character set count by 1
     $CharacterSet += $UpperCaseCharacters
+    $CharacterSetCount++
 
 }
 
 If ($Numbers) {
 
+    #If it has, add it to the character set and increment the character set count by 1
     $CharacterSet += $NumberCharacters
+    $CharacterSetCount++
 
 }
 
 If ($Symbols) {
 
+    #If it has, add it to the character set and increment the character set count by 1
     $CharacterSet += $SymbolCharacters
+    $CharacterSetCount++
 
+}
+
+#Ensure the (minimum) length is greater than or equal to the number of character set counts. Otherwise verification will fail later.
+If ($Length[0] -lt $CharacterSetCount) {
+
+    Write-Error "The specified length of the password must be greater than or equal to the number of character sets chosen!"
+    Break
+    
 }
 
 If ($ExcludeAmbiguousCharacters) {
