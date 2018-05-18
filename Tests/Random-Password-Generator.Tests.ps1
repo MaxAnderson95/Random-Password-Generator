2a478de4e501c29512f80cf6165690d25d98ac5a
Max Anderson <cctpmax@gmail.com>
2018-05-18 19:42:10 -0400

    Fixed test script

--- Tests/Random-Password-Generator.Tests.ps1
+++ Tests/Random-Password-Generator.Tests.ps1
@@ -1,5 +1,12 @@
-Import-Module "..\Random-Password-Generator"
+$Verbose = @{}
+if ($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master") {
+    
+    $Verbose.add("Verbose",$True)
 
+}
+
+$PSVersion = $PSVersionTable.PSVersion.Major
+Import-Module $PSScriptRoot\..\Random-Password-Generator -Force
 Describe "New-RandomPassword" {
 
     #Context "No Parameters" {
