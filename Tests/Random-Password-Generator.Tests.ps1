e9d7f8a194d4a3b160ba4d6eab008f5725bcf7fb
Max Anderson <cctpmax@gmail.com>
2018-05-18 19:09:18 -0400

    Added tests.

---
+++ Tests/Random-Password-Generator.Tests.ps1
@@ -0,0 +1,104 @@
+Import-Module "..\Random-Password-Generator"
+
+Describe "New-RandomPassword" {
+
+    #Context "No Parameters" {
+
+        It "Errors if no parameters are given" {
+
+            $Error.clear()
+            New-RandomPassword -ErrorAction SilentlyContinue
+            $Error.Exception.Message | Should Be "At least one character group must be chosen!"
+
+        }
+
+        It "Errors if number of character classes exceeds specified length" {
+
+            $Error.clear()
+            New-RandomPassword -LowerCase -UpperCase -Numbers -Length 2 -ErrorAction SilentlyContinue
+            $Error.Exception.Message | Should Be "The specified length of the password must be greater than or equal to the number of character sets chosen!"
+
+        }
+
+        It "No length is specified, a 10 character password is outputted" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -LowerCase -UpperCase -Numbers -Symbols
+            $Password.length | Should Be 10
+
+        }
+
+        It "If length is specified, the password outputted is that length" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -LowerCase -UpperCase -Numbers -Symbols -Length 14
+            $Password.length | Should Be 14
+
+        }
+
+        It "If password count is specified, the number of outputted passwords matches the count" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -LowerCase -UpperCase -Numbers -Symbols -Count 2
+            $Password.count | Should Be 2
+
+        }
+
+        It "If -LowerCase is specified, the password contains at least 1 lower case letter" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -LowerCase
+            $Password | Should MatchExactly '[a-z]'
+
+        }
+
+        It "If -UpperCase is specified, the password contains at least 1 upper case letter" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -UpperCase
+            $Password | Should MatchExactly '[A-Z]'
+
+        }
+
+        It "If -Numbers is specified, the password contains at least 1 number" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -Numbers
+            $Password | Should MatchExactly '\d'
+
+        }
+
+        It "If -Symbols is specified, the password contains at least 1 number" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -Symbols -ErrorAction SilentlyContinue                
+            $Password | Should Match "(\p{P}|\p{S})+"
+        
+        }
+
+        <#
+        It "If -ExcludeAmbiguousCharacters is specified, the password should not contain any" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -Symbols -ExcludeAmbiguousCharacters -ErrorAction SilentlyContinue
+            ForEach ($Char in $Password.ToCharArray()) {
+
+                @('(',')','[',']','{','}','/','\','.',',','0','O','o','Q') | Should Contain $Char
+
+            }
+
+        }
+        #>
+
+        It "If -CopyToClipboard is specified, the clipboard should contain the password" {
+
+            Remove-Variable Password -ErrorAction SilentlyContinue
+            $Password = New-RandomPassword -LowerCase -UpperCase -Numbers -Symbols -CopyToClipboard -ErrorAction SilentlyContinue
+            $Clipboard = Get-Clipboard
+            $Clipboard | Should BeExactly $Password
+
+        }
+
+    #}
+
+}
